cde07aaa102a1990fa8c202a78418985872365ed
guoyao <guoyao@uniontech.com>
2022-09-14 17:49:17 +0800

    feat: change demo to io
    change demo to io
    
    Log:

--- include/namespace.h
+++ include/namespace.h
@@ -2,9 +2,9 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-#define DDEMO_NAMESPACE Dtk::Demo
+#define DIO_NAMESPACE Dtk::IO
 
-#define DDEMO_USE_NAMESPACE using namespace DDEMO_NAMESPACE;
+#define DIO_USE_NAMESPACE using namespace DIO_NAMESPACE;
 
-#define DDEMO_BEGIN_NAMESPACE namespace Dtk { namespace Demo {
-#define DDEMO_END_NAMESPACE }}
+#define DIO_BEGIN_NAMESPACE namespace Dtk { namespace IO {
+#define DIO_END_NAMESPACE }}
