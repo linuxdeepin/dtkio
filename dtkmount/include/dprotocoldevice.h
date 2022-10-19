95001ca305c8aef14b2a541d74d232d59a76e422
XuSt <50353838+itsXuSt@users.noreply.github.com>
2022-10-19 14:05:07 +0800

    feat: [mount] implement functions of protocol monitor. (#2)
    * feat: [mount] implement functions of protocol monitor.
    
    as title.
    
    Log: feature implement.
    
    * feat: [mount] impl some functions of protocol.
    
    as title.
    
    Log: impl some functions of protocol.
    
    Task: https://pms.uniontech.com/task-view-201235.html
    
    * feat: [mount] block device monitor function implementation
    
    as title.
    
    Log: functions implementation
    
    Task: https://pms.uniontech.com/task-view-201235.html

---
+++ dtkmount/include/dprotocoldevice.h
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef DPROTOCOLDEVICE_H
+#define DPROTOCOLDEVICE_H
+
+#include <functional>
+
+#include <QObject>
+#include <QVariantMap>
+
+#include "dtkmount_global.h"
+
+DMOUNT_BEGIN_NAMESPACE
+
+enum PasswdSave {
+    Never = 0,
+    SaveInSession,
+    SaveForever
+};
+
+namespace MountPasswdInfoParamKeys {
+inline constexpr char kName[] { "name" };
+inline constexpr char kPasswd[] { "passwd" };
+inline constexpr char kDomain[] { "domain" };
+inline constexpr char kAnonymous[] { "anonymous" };
+inline constexpr char kCancel[] { "cancel" };
+inline constexpr char kSaveMode[] { "mode" };
+}   // namespace MountPasswdInfoParamKeys
+
+// TODO(xust): error handle in callback
+using OperateCallback = std::function<void(bool)>;
+using OperateCallbackWithInfo = std::function<void(bool, const QString &)>;
+
+using AskForPasswd = std::function<QVariantMap(const QString &msg, const QString &user, const QString &domain)>;
+using AskForChoice = std::function<int(const QString &msg, const QStringList &choices)>;
+
+class DProtocolDevicePrivate;
+class DProtocolDevice : public QObject
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(DProtocolDevice)
+
+public:
+    explicit DProtocolDevice(const QString &devicePath, QObject *parent = nullptr);
+    virtual ~DProtocolDevice();
+
+    QString path() const;
+    QString mountPoint() const;
+    QString displayName() const;
+    QString fileSystem() const;
+    quint64 sizeTotal() const;
+    quint64 sizeFree() const;
+    quint64 sizeUsage() const;
+    QStringList iconNames() const;
+
+    QString mount(const QVariantMap &opts = {});
+    void mountAsync(const QVariantMap &opts = {}, OperateCallbackWithInfo callback = nullptr);
+    bool unmount(const QVariantMap &opts = {});
+    void unmountAsync(const QVariantMap &opts = {}, OperateCallback callback = nullptr);
+
+    void setOperateTimeout(int secs = 3);
+    void setAskForPasswd(AskForPasswd callback);
+    void setAskForChoice(AskForChoice callback);
+
+private:
+    QScopedPointer<DProtocolDevicePrivate> d_ptr;
+};
+
+DMOUNT_END_NAMESPACE
+
+#endif   // DPROTOCOLDEVICE_H
---
+++ dtkmount/include/dprotocoldevicemonitor.h
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef DPROTOCOLDEVICEMONITOR_H
+#define DPROTOCOLDEVICEMONITOR_H
+
+#include <QObject>
+
+#include "dtkmount_global.h"
+
+DMOUNT_BEGIN_NAMESPACE
+
+class DProtocolDeviceMonitorPrivate;
+class DProtocolDeviceMonitor : public QObject
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(DProtocolDeviceMonitor)
+
+public:
+    explicit DProtocolDeviceMonitor(QObject *parent = nullptr);
+    virtual ~DProtocolDeviceMonitor();
+
+    void setWatchChanges(bool watchChanges);
+    bool watchChanges() const;
+    QStringList devices() const;
+
+Q_SIGNALS:
+    void deviceAdded(const QString &devUrl);
+    void deviceRemoved(const QString &devUrl);
+    void mountAdded(const QString &devUrl, const QString &mountPoint);
+    void mountRemoved(const QString &devUrl);
+
+private:
+    QScopedPointer<DProtocolDeviceMonitorPrivate> d_ptr;
+};
+
+DMOUNT_END_NAMESPACE
+
+#endif   // DPROTOCOLDEVICEMONITOR_H
