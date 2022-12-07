d83c7e61d3a79a0f2c1d1f88e3e74ebf1eba9d0a
renzis <hhsdsryx@qq.com>
2022-12-07 13:13:52 +0800

    feat: [dtkmount] gui monitor
    A device monitor program with GUI based on `dtkmount`
    
    Log: Model/View version

---
+++ dtkmount/examples/device_monitor/include/itemdelegate.h
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef ITEMDELEGATE_H
+#define ITEMDELEGATE_H
+
+#include <QModelIndex>
+#include <QStandardItemModel>
+#include <QStyledItemDelegate>
+
+class ItemDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+signals:
+
+public:
+    explicit ItemDelegate(QObject *parent = 0);
+    ~ItemDelegate();
+
+    // 重写绘画函数
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
+
+private:
+    static QStringList m_unit;
+};
+
+#endif  // ITEMDELEGATE_H
---
+++ dtkmount/examples/device_monitor/include/sortproxy.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef SORTPROXY_H
+#define SORTPROXY_H
+#include <QSortFilterProxyModel>
+
+class SortProxy : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    SortProxy(QObject *parent = nullptr);
+    ~SortProxy();
+
+protected:
+    bool lessThan(const QModelIndex &sourceLeft, const QModelIndex &sourceRight) const override;
+};
+#endif  // SORTPROXY_H
