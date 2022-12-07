e7405bbcd3367d8cdc34309d7ffb69a1639dc7ee
Zhang Sheng <zhangsheng@uniontech.com>
2022-12-07 17:18:52 +0800

    feat: [burn] Impl interfaces
    Supported public interfaces by `DOpticalDiscOperator` and `DOpticalDiscInfo`. Core capabilities are realized by `DXorrisoEngine` and `DUDFBurnEngine`.
    
    Log: Impl interfaces
    
    Task: https://pms.uniontech.com/task-view-223193.html

--- dtkburn/src/dopticaldiscmanager.cpp
+++ dtkburn/src/dopticaldiscmanager.cpp
@@ -2,22 +2,34 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-#include "doptifcaldiscmanager.h"
+#include "dopticaldiscmanager.h"
+
+#include <DError>
+
+#include "dopticaldiscinfo.h"
+#include "dopticaldiscoperator.h"
 
 DBURN_BEGIN_NAMESPACE
 
+using DTK_CORE_NAMESPACE::DError;
 using DTK_CORE_NAMESPACE::DExpected;
+using DTK_CORE_NAMESPACE::DUnexpected;
 
 DExpected<DOpticalDiscInfo *> DOpticalDiscManager::createOpticalDiscInfo(const QString &dev, QObject *parent)
 {
-    // TODO(zhangs): impl me!
-    return {};
+    auto info { new DOpticalDiscInfo(dev, parent) };
+    if (info && info->device().isEmpty()) {
+        delete info;
+        return DUnexpected<> { DError { -1, QString("Invalid device: %1").arg(dev) } };
+    }
+    return info;
 }
 
 DExpected<DOpticalDiscOperator *> DOpticalDiscManager::createOpticalDiscOperator(const QString &dev, QObject *parent)
 {
-    // TODO(zhangs): impl me!
-    return {};
+    if (dev.isEmpty())
+        return DUnexpected<> { DError { -1, "Empty device" } };
+    return { new DOpticalDiscOperator(dev, parent) };
 }
 
 DBURN_END_NAMESPACE
